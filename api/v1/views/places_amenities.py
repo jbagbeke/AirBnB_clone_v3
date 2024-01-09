c8a97391818314c513ac9b97b8f4753881e6abc7
Joshua Bubune <joshua.legithive@gmail.com>
2024-01-09 23:33:38 +0000

    0x05. AirBnB clone - RESTful API Mandatory task 13

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -9,7 +9,7 @@ from models.amenity import Amenity
 from flask import jsonify, make_response
 
 
-@app_views.route('/api/v1/places/<place_id>/amenities',
+@app_views.route('/places/<place_id>/amenities',
                  strict_slashes=False,
                  methods=['GET'])
 def hbnb_place_amenity(place_id):
@@ -20,18 +20,18 @@ def hbnb_place_amenity(place_id):
 
     if not place_obj:
         abort(404)
-    place_amenity_ids = place_obj.amenity_ids
 
-    place_amenities = [storage.get(Amenity, amenity_id).to_dict()
-                       for amenity_id in place_amenity_ids]
+
+    place_amenities = [amenity_obj.to_dict()
+                       for amenity_obj in place_obj.amenities]
 
     return jsonify(place_amenities)
 
 
-@app_views.route('/api/v1/places/<place_id>/amenities/<amenity_id>',
+@app_views.route('/places/<place_id>/amenities/<amenity_id>',
                  strict_slashes=False,
                  methods=['DELETE'])
-def hbnb_amenity_delete(place_id, amenity_id):
+def hbnb_amenity_places(place_id, amenity_id):
     """
         Deletes a Amenity object to a Place
                                             """
@@ -40,23 +40,21 @@ def hbnb_amenity_delete(place_id, amenity_id):
     if not place_obj:
         abort(404)
 
-    place_amenity_ids = place_obj.amenity_ids
-
-    if amenity_id not in place_amenity_ids:
-        abort(404)
-
     amenity_obj = storage.get(Amenity, amenity_id)
 
-    if not amenity_obj:
+    if amenity_obj:
         abort(404)
 
-    return jsonify({})
+    if amenity_obj not in place_obj.amenities:
+        abort(404)
+
+    return make_response(jsonify({}), 200)
 
 
-@app_views.route('/api/v1/places/<place_id>/amenities/<amenity_id>',
+@app_views.route('/places/<place_id>/amenities/<amenity_id>',
                  strict_slashes=False,
                  methods=['POST'])
-def hbnb_amenity_post(place_id, amenity_id):
+def hbnb_places_obj(place_id, amenity_id):
     """
         Link a Amenity object to a Place
                                         """
@@ -70,15 +68,11 @@ def hbnb_amenity_post(place_id, amenity_id):
     if not amenity_obj:
         abort(404)
 
-    place_amenity_ids = place_obj.amenity_ids
-
-    if amenity_id in place_amenity_ids:
+    if amenity_obj in place_obj.amenities:
         return make_response(jsonify(amenity_obj.to_dict()), 200)
 
-    place_obj.amenity_ids = place_amenity_ids.append(amenity_id)
-    place_obj.amenities = amenity_obj
+    place_obj.amenities.append(amenity_obj)
 
     storage.save()
 
     return make_response(jsonify(amenity_obj.to_dict()), 201)
-
