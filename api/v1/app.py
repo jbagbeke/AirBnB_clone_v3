6e398dd5210fa817c6d0fcd623595c05ed802b4c
Joshua Bubune <joshua.legithive@gmail.com>
2024-01-09 02:53:27 +0000

    0x05. AirBnB clone - RESTful API Mandatory task 12

--- api/v1/app.py
+++ api/v1/app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
-cors = CORS(app, resources={r"api/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
